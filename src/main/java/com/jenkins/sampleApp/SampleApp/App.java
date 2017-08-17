c15ef1c145dee625bad7f1e99f08257113a578ab
Nazeena MD <mohammad_nazeena@optum.com>
2017-08-17 14:02:24 +0530

    first clone

---
+++ src/main/java/com/jenkins/sampleApp/SampleApp/App.java
@@ -0,0 +1,14 @@
+package com.jenkins.sampleApp.SampleApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello!" );
+    }
+}
